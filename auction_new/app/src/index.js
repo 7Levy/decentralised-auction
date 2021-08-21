e8dc298871ac0b893b5417b75328d797e777d3fe
levy <15958732798@163.com>
2021-08-21 13:33:22 +0800

    新版文件，前端搁置

--- auction_new/app/src/index.js
+++ auction_new/app/src/index.js
@@ -1,76 +1,25 @@
-import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import {default as Web3} from "web3";
+import {default as contract} from "truffle-contract"
+import product_store_artifacts from '../../build/contracts/ProductStore.json'
 
-const App = {
-  web3: null,
-  account: null,
-  meta: null,
+var ProductStroe = contract(product_store_artifacts);
 
-  start: async function() {
-    const { web3 } = this;
+var ipfsApi = require('ipfs-http-client')
 
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
-        deployedNetwork.address,
-      );
+var ipfs = ipfsApi({host:'localhost',port:'5001',protocol:'http'});
 
-      // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
 
-      this.refreshBalance();
-    } catch (error) {
-      console.error("Could not connect to contract or chain.");
-    }
-  },
-
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
-
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
-  },
-
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
-  },
-
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-};
-
-window.App = App;
-
-window.addEventListener("load", function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
-  } else {
-    console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    );
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    );
+window.App = {
+  start : function(){
+    var self = this;
   }
+}
 
+window.addEventListener('load',function(){
+  if(typeof web3 !=undefined){
+    window.web3 = new Web3(web3.currentProvider);
+  }else{
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  }
   App.start();
 });
--- auction_new/migrations/2_deploy_contracts.js
+++ auction_new/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+var ProductStore = artifacts.require('ProductStore');
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(ProductStore)
 };
